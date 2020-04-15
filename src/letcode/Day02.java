f63326ee3643708e3e00abb52af275e441caad44
xiyanqiu <798726768@qq.com>
2020-04-15 17:24:15 +0800

    开发人员第一次提交

---
+++ src/letcode/Day02.java
@@ -0,0 +1,14 @@
+package letcode;
+
+/**
+ * @Auther:leeling
+ * @Date:2020/4/15 Description:letcode
+ */
+public class Day02 {
+    //开发人员
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+}
