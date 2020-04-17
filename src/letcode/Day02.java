b0785963b0b571cdd0e6fc26afd7a025b950a394
leeling <857579908@qq.com>
2020-04-17 15:49:27 +0800

    EVERYDAY

--- src/letcode/Day02.java
+++ src/letcode/Day02.java
@@ -10,6 +10,25 @@ import java.util.Scanner;
  */
 public class Day02 {
 
+    public int numJewelsInStones(String J, String S) {
+        int count =0;
+        char[] baoshi = J.toCharArray();
+        for(int i =0;i<S.length();i++)
+        {
+            char eve = S.charAt(i);
+            for(int j =0;j<baoshi.length;j++)
+            {
+                if(eve == baoshi[j])
+                {
+                    count = count+1;
+
+                }
+            }
+
+        }
+            return count;
+    }
+
 //    public String addStrings(String num1, String num2) {
 //        char[] nums1 = num1.toCharArray();
 //        char[] nums2 = num2.toCharArray();
@@ -21,6 +40,28 @@ public class Day02 {
 //
 //
 //    }
+
+    /**
+     * 整数的各位积和之差
+     * @param n
+     * @return
+     */
+    public int subtractProductAndSum(int n) {
+        int plus = 1;
+        int sum = 0;
+        //数字转字符串
+        String s = String.valueOf(n);
+        //字符串操作
+        for(int i=0;i<s.length();i++)
+        {
+
+            char everywei = s.charAt(i);
+            plus = Integer.parseInt(String.valueOf(everywei)) * plus;
+            sum = sum+Integer.parseInt(String.valueOf(everywei));
+        }
+        return plus-sum;
+
+    }
     //按位翻转字符串
     public String reverseLeftWords(String s, int n) {
         //如果指定的位数为负或0
@@ -88,11 +129,16 @@ public class Day02 {
 //        int num = 123;
 //        int ret = day02.numberOfSteps(num);
 //        System.out.println(ret);
-        String s = "abcdefg";
-        int n = 2;
-        String ret = day02.reverseLeftWords(s, n);
-        System.out.println(ret);
-
+//        String s = "abcdefg";
+//        int n = 2;
+//        String ret = day02.reverseLeftWords(s, n);
+//        System.out.println(ret);
+//        int n = 4421;
+//        int ret = day02.subtractProductAndSum(n);
+//        System.out.println(ret);
+          String J = "z", S = "ZZ";
+          int ret = day02.numJewelsInStones(J, S);
+          System.out.println(ret);
 
 
     }
